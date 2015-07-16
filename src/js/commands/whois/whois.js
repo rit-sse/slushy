24b1556a44baca767a5f121f67f295bc128e0427
Matt Mokary <mxm6060@rit.edu>
2015-07-16 15:16:04 -0400

    moved whois data into json file, added position to whois descriptions because things like 'pr' and 'mentoring' should be more descriptive

--- src/js/commands/whois/whois.js
+++ src/js/commands/whois/whois.js
@@ -1,30 +1,13 @@
-// until we get all that DB stuff figured out
-var data = {
-    'fall15' : {
-        'president' : 'John Renner (president@sse.se.rit.edu)',
-        'vice president' : 'Brian Besmanoff (vp@sse.se.rit.edu)',
-        'treasurer' : 'Ben Kantor (treasurer@sse.se.rit.edu)',
-        'secretary' : 'Jesse Jurman (secretary@sse.se.rit.edu)',
-        'mentoring' : 'Matt Mokary (mentoring@sse.se.rit.edu)',
-        'projects' : 'John Grischuk (projects@sse.se.rit.edu)',
-        'events' : 'Wade Mauger (events@sse.se.rit.edu)',
-        'fundraising' : 'Justin Peterson (fundraising@sse.se.rit.edu)',
-        'technology' : 'Craig Cabrey (technology@sse.se.rit.edu)',
-        'winter ball' : 'Kayla Nussbaum (winterball@sse.se.rit.edu)',
-        'pr' : 'Dominic Cicilio (pr@sse.se.rit.edu)',
-        'lab ops' : 'Matt Smicinski (labops@sse.se.rit.edu)'
-    }
-};
-
-var CURRENT_TERM = 'fall15';
+// until we get DB stuff figured out
+var data = require('./whois.json');
 
 var api = {
     run : function (metadata, args) {
         var position = args.join(' '),
             returnText = '';
 
-        if (data[CURRENT_TERM][position]) {
-            returnText = position + ': ' + data[CURRENT_TERM][position]
+        if (data[data.CURRENT_TERM][position]) {
+            returnText = data[data.CURRENT_TERM][position]
         } else {
             returnText = 'Unrecognized position: ' + position +
                 '. Type `/sse help whois` to see all positions.';
